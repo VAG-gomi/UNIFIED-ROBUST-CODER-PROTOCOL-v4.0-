9e1ca28503918ebcde1f923db28c8750a7858108
VAG-gomi <polerbear939@gmail.com>
2026-07-26 08:16:49 +0000

    Create version.py

---
+++ version.py
@@ -0,0 +1,14 @@
+"""
+core_neuron.version
+===================
+Single source of truth for the package version.
+
+Follows `Semantic Versioning 2.0.0 <https://semver.org/>`_:
+``MAJOR.MINOR.PATCH[-pre-release][+build]``.
+"""
+
+#: Current release version.
+__version__: str = "0.1.0"
+
+#: Minimum Python version required (major, minor).
+MIN_PYTHON: tuple[int, int] = (3, 10)
